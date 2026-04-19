382cec0927f95077d75008ee842ba0fc105bbf6d
Fabian Lucena <85037034+fabianlucena@users.noreply.github.com>
2026-04-19 13:36:39 -0300

    feat: complete login and session creation

--- FreeCampusServer/DBScripts/create.sql
+++ FreeCampusServer/DBScripts/create.sql
@@ -1,64 +1,116 @@
-CREATE TABLE auth.Users (
-    Id BIGINT IDENTITY(1,1) NOT NULL PRIMARY KEY,
-    Uuid UNIQUEIDENTIFIER NOT NULL,
-    CreatedAt DATETIME2 NOT NULL,
-    UpdatedAt DATETIME2 NOT NULL,
-    DeletedAt DATETIME2 NULL,
-    CreatedById BIGINT NOT NULL,
-    UpdatedById BIGINT NOT NULL,
-    DeletedById BIGINT NULL,
-
-    Username VARCHAR(64) NOT NULL,
-    DisplayName VARCHAR(128) NOT NULL,
-    Email VARCHAR(256) NOT NULL,
-    PasswordHash VARCHAR(256) NOT NULL,
-    IsActive BIT NOT NULL,
-    CanLogin BIT NOT NULL,
-    LastLogin DATETIME2 NULL
-);
-
-INSERT INTO auth.Users (
-    Uuid, CreatedAt, UpdatedAt, DeletedAt,
-    CreatedById, UpdatedById, DeletedById,
-    Username, DisplayName, Email, PasswordHash,
-    IsActive, CanLogin, LastLogin
+IF NOT EXISTS (
+    SELECT 1
+    FROM sys.tables t
+    JOIN sys.schemas s ON t.schema_id = s.schema_id
+    WHERE t.name = 'Users'
+      AND s.name = 'auth'
 )
-VALUES (
-	NEWID(), GETUTCDATE(), GETUTCDATE(), NULL,
-	0, 0, NULL,
-	'system', 'System', '', '',
-	1, 1, NULL
-);
+BEGIN
+	CREATE TABLE auth.Users (
+		Id BIGINT IDENTITY(1,1) NOT NULL PRIMARY KEY,
+		Uuid UNIQUEIDENTIFIER NOT NULL,
+		CreatedAt DATETIME2 NOT NULL,
+		UpdatedAt DATETIME2 NOT NULL,
+		DeletedAt DATETIME2 NULL,
+		CreatedById BIGINT NOT NULL,
+		UpdatedById BIGINT NOT NULL,
+		DeletedById BIGINT NULL,
 
-DECLARE @Id BIGINT = SCOPE_IDENTITY();
-UPDATE auth.Users
-SET CreatedById = @Id,
-    UpdatedById = @Id
-WHERE Id = @Id;
+		Username VARCHAR(64) NOT NULL,
+		DisplayName VARCHAR(128) NOT NULL,
+		Email VARCHAR(256) NOT NULL,
+		PasswordHash VARCHAR(256) NOT NULL,
+		IsActive BIT NOT NULL,
+		CanLogin BIT NOT NULL,
+		LastLogin DATETIME2 NULL
+	);
 
-ALTER TABLE auth.Users
-ADD CONSTRAINT FK_Users_CreatedBy
-    FOREIGN KEY (CreatedById) REFERENCES auth.Users(Id) ON DELETE NO ACTION;
+	INSERT INTO auth.Users (
+		Uuid, CreatedAt, UpdatedAt, DeletedAt,
+		CreatedById, UpdatedById, DeletedById,
+		Username, DisplayName, Email, PasswordHash,
+		IsActive, CanLogin, LastLogin
+	)
+	VALUES (
+		NEWID(), GETUTCDATE(), GETUTCDATE(), NULL,
+		0, 0, NULL,
+		'system', 'System', '', '',
+		1, 1, NULL
+	);
 
-ALTER TABLE auth.Users
-ADD CONSTRAINT FK_Users_UpdatedBy
-    FOREIGN KEY (UpdatedById) REFERENCES auth.Users(Id) ON DELETE NO ACTION;
+	DECLARE @Id BIGINT = SCOPE_IDENTITY();
+	UPDATE auth.Users
+	SET CreatedById = @Id,
+		UpdatedById = @Id
+	WHERE Id = @Id;
 
-ALTER TABLE auth.Users
-ADD CONSTRAINT FK_Users_DeletedBy
-    FOREIGN KEY (DeletedById) REFERENCES auth.Users(Id) ON DELETE NO ACTION;
+	ALTER TABLE auth.Users
+	ADD CONSTRAINT FK_Users_CreatedBy
+		FOREIGN KEY (CreatedById) REFERENCES auth.Users(Id) ON DELETE NO ACTION;
 
+	ALTER TABLE auth.Users
+	ADD CONSTRAINT FK_Users_UpdatedBy
+		FOREIGN KEY (UpdatedById) REFERENCES auth.Users(Id) ON DELETE NO ACTION;
+
+	ALTER TABLE auth.Users
+	ADD CONSTRAINT FK_Users_DeletedBy
+		FOREIGN KEY (DeletedById) REFERENCES auth.Users(Id) ON DELETE NO ACTION;
+END
 GO
 
-CREATE TABLE auth.Devices (
-    Id BIGINT IDENTITY(1,1) NOT NULL PRIMARY KEY,
-    Uuid UNIQUEIDENTIFIER NOT NULL,
+IF NOT EXISTS (
+    SELECT 1
+    FROM sys.tables t
+    JOIN sys.schemas s ON t.schema_id = s.schema_id
+    WHERE t.name = 'Devices'
+      AND s.name = 'auth'
+)
+BEGIN
+	CREATE TABLE auth.Devices (
+		Id BIGINT IDENTITY(1,1) NOT NULL PRIMARY KEY,
+		Uuid UNIQUEIDENTIFIER NOT NULL,
 
-    CreatedAt DATETIME2 NOT NULL,
-    CreatedById BIGINT NOT NULL,
+		CreatedAt DATETIME2 NOT NULL,
+		CreatedById BIGINT NOT NULL,
 
-    Token VARCHAR(64) NOT NULL,
+		Token VARCHAR(64) NOT NULL,
 
-    CONSTRAINT FK_Devices_CreatedBy FOREIGN KEY (CreatedById)
-        REFERENCES auth.Users(Id) ON DELETE NO ACTION,
-);
+		CONSTRAINT FK_Devices_CreatedBy FOREIGN KEY (CreatedById)
+			REFERENCES auth.Users(Id) ON DELETE NO ACTION,
+	);
+END
+GO
+
+IF NOT EXISTS (
+    SELECT 1
+    FROM sys.tables t
+    JOIN sys.schemas s ON t.schema_id = s.schema_id
+    WHERE t.name = 'Sessions'
+      AND s.name = 'auth'
+)
+BEGIN
+	CREATE TABLE auth.Sessions (
+		Id BIGINT IDENTITY(1,1) NOT NULL PRIMARY KEY,
+		Uuid UNIQUEIDENTIFIER NOT NULL,
+
+		CreatedAt DATETIME2 NOT NULL,
+		CreatedById BIGINT NOT NULL,
+
+		Token VARCHAR(64) NOT NULL,
+		ExpireAt DATETIME2 NOT NULL,
+		AutoLoginToken VARCHAR(64) NOT NULL,
+		
+		UserId BIGINT NOT NULL,
+		DeviceId BIGINT NOT NULL,
+
+		CONSTRAINT FK_Sessions_CreatedBy FOREIGN KEY (CreatedById)
+			REFERENCES auth.Users(Id) ON DELETE NO ACTION,
+		
+		CONSTRAINT FK_Sessions_User FOREIGN KEY (UserId)
+			REFERENCES auth.Users(Id) ON DELETE NO ACTION,
+			
+		CONSTRAINT FK_Sessions_Device FOREIGN KEY (DeviceId)
+			REFERENCES auth.Devices(Id) ON DELETE NO ACTION,
+	);
+END
+GO
---
+++ FreeCampusServer/RCBACEF/EntityTypeConfigurations/SessionConfiguration.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using RCBACEF.Models;
+
+namespace RCBACEF.EntityTypeConfigurations
+{
+    public class SessionConfiguration : BaseConfiguration<Session>
+    {
+        public override void Configure(EntityTypeBuilder<Session> entity)
+        {
+            base.Configure(entity);
+        }
+    }
+}
