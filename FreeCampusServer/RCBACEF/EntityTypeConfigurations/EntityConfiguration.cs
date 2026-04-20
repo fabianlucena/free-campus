4ecab43251c0cbc8aa2b6cb312c675b2fb53eb21
Fabian Lucena <85037034+fabianlucena@users.noreply.github.com>
2026-04-19 23:19:12 -0300

    fix: remove redundant using directive for Microsoft.EntityFrameworkCore

--- FreeCampusServer/RCBACEF/EntityTypeConfigurations/EntityConfiguration.cs
+++ FreeCampusServer/RCBACEF/EntityTypeConfigurations/EntityConfiguration.cs
@@ -1,5 +1,4 @@
-﻿using Microsoft.EntityFrameworkCore;
-using Microsoft.EntityFrameworkCore.Metadata.Builders;
+﻿using Microsoft.EntityFrameworkCore.Metadata.Builders;
 using RCBACEF.Models;
 
 namespace RCBACEF.EntityTypeConfigurations
