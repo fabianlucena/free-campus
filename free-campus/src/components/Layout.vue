276bd6d4e08ed3e6125fa44fa23be63de4778684
Fabian Lucena <85037034+fabianlucena@users.noreply.github.com>
2026-04-21 17:03:13 -0300

    feat: refactor Layout and MainMenu components for improved structure and styling

--- free-campus/src/components/Layout.vue
+++ free-campus/src/components/Layout.vue
@@ -2,19 +2,29 @@
   <Header
     @toggleMenu="showMenu = !showMenu"
   />
-  <MenuBar
-    v-if="showMenu"
-    :items="menuItems"
-  />
-  <Body />
+  <div class="main">
+    <MainMenu
+      v-if="showMenu"
+      :items="menuItems"
+    />
+    <Body />
+  </div>
 </template>
 
 <script setup>
 import { ref } from 'vue';
-import MenuBar from '@vc/MenuBar.vue';
+import MainMenu from './MainMenu.vue';
 import Header from './Header.vue';
 import Body from './Body.vue';
 import { menuItems } from '../services/menu-items.js';
 
 const showMenu = ref(true);
 </script>
+
+<style>
+.main {
+  display: flex;
+  flex: 1;
+  overflow: auto;
+}
+</style>
--- free-campus/src/components/MainMenu.vue
+++ free-campus/src/components/MainMenu.vue
@@ -1,10 +1,17 @@
 <template>
-  <MenuBar :items="menuItems" />
+  <MenuBar
+    class="vertical"
+    :items="menuItems"
+  />
 </template>
 
 <script setup>
 import MenuBar from '@vc/MenuBar.vue';
 import { menuItems } from '../services/menu-items.js';
-
-//
 </script>
+
+<style scoped>
+nav {
+  height: 100%;
+}
+</style>
